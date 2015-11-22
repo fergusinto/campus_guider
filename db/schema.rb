17bd9f0823b7632686a4b1d2c9ca7312f559d143
CarlosLin <weihung0418@gmail.com>
2015-11-22 10:09:44 +0800

    Fix devise strong parameter and drop admin table

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119070941) do
-
-  create_table "admins", force: :cascade do |t|
-    t.string   "email",              default: "", null: false
-    t.string   "encrypted_password", default: "", null: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-  end
-
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+ActiveRecord::Schema.define(version: 20151121152445) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
